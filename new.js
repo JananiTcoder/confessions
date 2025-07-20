cccba679c1b239fa75f78ed758574e302f2bb328
Janani T <jananiroshini2005@gmail.com>
2025-07-20 23:57:05 +0530

    new.js

--- new.js
+++ new.js
@@ -1,12 +1,1 @@
-  const firebaseConfig = {
-    apiKey: "AIzaSyBD0ICw9kuM9Td_naK5_eMF9pBuIjWVEGw",
-    authDomain: "ecell-suggestion.firebaseapp.com",
-    databaseURL: "https://ecell-suggestion-default-rtdb.firebaseio.com",
-    projectId: "ecell-suggestion",
-    storageBucket: "ecell-suggestion.firebasestorage.app",
-    messagingSenderId: "963694224214",
-    appId: "1:963694224214:web:b3cc06e97b212fdef26cf7",
-    measurementId: "G-GMP1N2849E"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const contactFormDB=firebase.database().ref("confessions")
+
